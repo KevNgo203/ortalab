ebb591c2dcb9bcfaa2a3e094f752621f61fdee38
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2025-10-29 14:14:48 +1100

    Added helper function for Flower Pot Joker which works with Smeared Joker

--- src/poker/helpers.rs
+++ src/poker/helpers.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use ortalib::{Card, Suit};
+use ortalib::{Card, Enhancement, Suit, SuitColor};
 
 // Helper function for Flush-based Hand, which computes the most appear suit
 pub fn compute_most_appear_suit(cards: &[Card]) -> Suit {
@@ -10,4 +10,30 @@ pub fn compute_most_appear_suit(cards: &[Card]) -> Suit {
         .max_by_key(|(_, count)| *count)
         .map(|(suit, _)| suit)
         .unwrap()
+}
+
+// Helper function for FLower Pot Joker, which determines whether the current 
+// card having the wanted suit
+pub fn determine_current_suit(on_scored: &[Card], suit: Suit) -> bool {
+    on_scored.iter().any(|c| {
+        if c.suit == suit {
+            return true;
+        } else if let Some(enhance) = c.enhancement 
+        && enhance == Enhancement::Wild {
+            return true;
+        };
+        false
+    })
+}
+
+pub fn determine_total_colors(on_scored: &[Card], color: SuitColor) -> bool {
+    on_scored.iter().filter(|card| {
+        if card.suit.color() == color {
+            return true;
+        } else if let Some(enhance) = card.enhancement 
+        && enhance == Enhancement::Wild {
+            return true;
+        };
+        false
+    }).count() >= 2
 }
