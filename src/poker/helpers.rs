3d282261bc1ea25b7ac300efb363882f6efa3279
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2025-10-29 13:06:45 +1100

    Moved helper function to a seperate file - helpers.rs

---
+++ src/poker/helpers.rs
@@ -0,0 +1,16 @@
+use itertools::Itertools;
+use ortalib::{Card, Suit};
+
+// Helper function for Flush-based Hand, which computes the most appear suit
+pub fn compute_most_appear_suit(cards: &[Card]) -> Suit {
+    cards
+        .iter()
+        .counts_by(|c| c.suit)
+        .into_iter()
+        .max_by_key(|(_, count)| *count)
+        .map(|(suit, _)| suit)
+        .unwrap()
+}
+
+// Helper function for determine the effect of Four Fingers Joker 
+// pub fn determine_four_fingers(return_card: Vec<Card>) -> 
--- src/poker/mod.rs
+++ src/poker/mod.rs
@@ -2,7 +2,9 @@ pub mod hands;
 pub mod jokers;
 pub mod modifiers;
 pub mod scoring;
+pub mod helpers;
 
 pub use hands::determine_poker_hand;
 pub use modifiers::apply_edition;
 pub use scoring::score;
+pub use helpers::{compute_most_appear_suit};
